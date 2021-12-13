4e13f43036b556cccfe4fafd9e111ac6d6ed6e90
Marcus Guinane <marcus.guinane@gmail.com>
2021-12-13 22:28:58 +0000

    Module 3 completed.

---
+++ src/components/Expenses/ExpenseItemDate.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./ExpenseItemDate.css";
+
+const ExpenseItemDate = (props) => {
+  const day = props.date.toLocaleString("en-GB", { day: "2-digit" });
+  const month = props.date.toLocaleString("en-GB", { month: "long" });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+    </div>
+  );
+};
+
+export default ExpenseItemDate;
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      {props.expenses.map((item) => (
+        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+      ))}
+    </Card>
+  );
+};
+
+export default Expenses;
